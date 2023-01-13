f435182083eb55681988039d94258b3beb2affea
daniel <daniel97.le@gmail.com>
2023-01-12 22:00:07 -0700

    better scaffold

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@sidebase/nuxt-auth',
+    '@nuxtjs/tailwindcss'
+  ],
+  typescript: {
+    shim: false
+  }
+})
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="text-4xl">
+      Welcome to your sidebase app!
+    </h1>
+    <p>
+      Checkout the page protected by `nuxt-auth` here:
+      <nuxt-link to="/protected" class="underline text-blue">
+        Click me to test the auth setup!
+      </nuxt-link>
+    </p>
+  </div>
+</template>
