f0c4e5f87026968b535713b9b1f0b4e5ae051b59
daniel <daniel97.le@gmail.com>
2023-01-14 11:15:22 -0700

    fixes

--- service/ProductsService.ts
+++ service/ProductsService.ts
@@ -2,31 +2,22 @@ import { AppState } from '~~/AppState'
 import { Product } from '~~/models/Products'
 
 class ProductsService {
-
   async getProducts () {
     const { find } = useStrapi()
 
     const res = await find<Product>('products')
-
     const products = res.data.map(r => new Product(r))
 
-
-
     AppState.products = products
-
-
   }
 
-  async getProductById(productId:string){
-
-// const res = await findOne<Product>('products',{id:String})
-
+  async getProductById (productId:string) {
+    const { findOne } = useStrapi()
+    const res = await findOne<Product>('products', productId)
 
     // AppState.activeProduct = res((r: any) => new Product(r))
 
-
-
-    // AppState.activeProduct = new Product(res)
+    AppState.activeProduct = new Product(res)
   }
 }
 export const productsService = new ProductsService()
