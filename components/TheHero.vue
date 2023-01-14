276f2f26d9543dffaa8ec8c4e9de361c12ba840a
daniel <daniel97.le@gmail.com>
2023-01-13 17:35:03 -0700

    setup

---
+++ components/TheHero.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+onMounted(() => {
+  getDogs()
+})
+async function getDogs () {
+  try {
+     const hi = await dogsService.get()
+  } catch (error) {
+    // Pop.error(error)
+  }
+}
+</script>
+
+<template>
+  <div>
+    Component: TheHero
+  </div>
+</template>
+
+<style scoped></style>
