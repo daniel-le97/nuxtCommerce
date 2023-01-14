3c08bbb5c7b68476019d30684180a89593caed8b
daniel <daniel97.le@gmail.com>
2023-01-13 20:52:30 -0700

    fix lint

--- store/cart.js
+++ store/cart.js
@@ -2,41 +2,38 @@ import { defineStore, acceptHMRUpdate } from 'pinia'
 
 export const cartStore = defineStore('cart', {
   state: () => ({
-
-      items: [],
-      total: 0,
-
+    items: [],
+    total: 0
   }),
   mutations: {
-    addProduct(state, product) {
+    addProduct (state, product) {
       state.items.push(product)
       state.total += product.price
-   
     },
-    removeProduct(state, index) {
+    removeProduct (state, index) {
       state.total -= state.items[index].price
       state.items.splice(index, 1)
     },
-    clearCart(state) {
+    clearCart (state) {
       state.items = []
       state.total = 0
-    },
+    }
   },
   actions: {
-    addToCart({commit}, product) {
+    addToCart ({ commit }, product) {
       commit('addProduct', product)
     },
-    removeFromCart({commit}, index) {
+    removeFromCart ({ commit }, index) {
       commit('removeProduct', index)
     },
-    clearCart({commit}) {
+    clearCart ({ commit }) {
       commit('clearCart')
     }
   },
   getters: {
-    cartItems: (state) => state.items,
-    cartTotal: (state) => state.total,
-    cartCount: (state) => state.items.length
+    cartItems: state => state.items,
+    cartTotal: state => state.total,
+    cartCount: state => state.items.length
   }
 })
 
